fc732a7e7a180b200509298058d8c92d3d1dab82
josepicanco <engpicanco@yahoo.com.br>
2026-03-09 17:13:09 -0300

    versao 3 desafio_dados

--- main.py
+++ main.py
@@ -3,39 +3,55 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-# pegando o arquivo que tá na pasta
+# localizando nossa base (esse arquivo deve estar na mesma pasta do codigo)
 arquivo_csv = 'titanic_dataset.csv'
 
-# conferir se o arquivo existe antes de dar erro
+# lendo o arquivo para verificar algum erro
 if os.path.exists(arquivo_csv):
     # lendo a tabela do titanic
     tabela = pd.read_csv(arquivo_csv)
 
-    # LIMPEZA DOS DADOS
-    # preenchendo as idades vazias com a mediana
+    # verificação para limpeza
+    print("--- RELATÓRIO DE DADOS ---")
+    total_antes = len(tabela)
+    nulos_por_coluna = tabela.isnull().sum()
+    
+    print(f"Total de passageiros no começo: {total_antes}")
+    print("Dados faltando por coluna:")
+    print(nulos_por_coluna)
+    print("-" * 30)
+
+    # Gráfico simples para mostrar o que estava "sujo" (nulos)
+    plt.figure(figsize=(8, 4))
+    nulos_por_coluna[nulos_por_coluna > 0].plot(kind='bar', color='orange')
+    plt.title('O que eu encontrei de dados nulos:')
+    plt.ylabel('Quantidade de buracos nos dados')
+    plt.show()
+
+    # preenchendo as idades vazias com a mediana (assim evita dados nulos)
     valor_do_meio = tabela['Age'].median()
     tabela['Age'] = tabela['Age'].fillna(valor_do_meio)
 
-    # tirando a coluna da cabine que tem muito erro
+    # tirando a coluna da cabine que tem muitoS NULOS
     tabela = tabela.drop(columns=['Cabin'])
 
-    # tirando linhas que não tem o porto de embarque
+    # tirando linhas que não tem o porto de embarque (são poucas não influenciam o resultado)
     tabela.dropna(subset=['Embarked'], inplace=True)
 
     # TRABALHANDO COM A FAMÍLIA (SibSp e Parch)
-    # vou somar os parentes pra ver quem tava sozinho
+    # somar os parentes pra ver quem tava sozinho
     tabela['Soma_Parentes'] = tabela['SibSp'] + tabela['Parch']
     
-    # lógica simples pra ver se está sozinho ou não
+    # critério para saber quem estava sozinho ou não
     tabela.loc[tabela['Soma_Parentes'] == 0, 'Como_Viajava'] = 'Sozinho'
     tabela.loc[tabela['Soma_Parentes'] > 0, 'Como_Viajava'] = 'Com Familia'
 
-    # PARTE DOS GRÁFICOS
+    # GRÁFICOS
     # Gráfico 1 - Sobrevivência por Classe e Sexo
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
     
-    # cores que o professor pediu: vermelho mulher e azul homem
+    # vermelho mulher e azul homem
     cores_lista = {"female": "red", "male": "blue"}
     
     sns.barplot(data=tabela, x='Pclass', y='Survived', hue='Sex', palette=cores_lista, errorbar=None)
